0b3bdc2f27e8c81fcdbbab33418d45fc77daf6c0
~venir <luca.venir@wde.it>
2025-09-30 12:20:12 +0200

    changed iterables to fit with FIC

--- lib/iterables.dart
+++ lib/iterables.dart
@@ -1,28 +1,27 @@
+import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 import 'package:its_aa_pn_2025_cross_platform/animal.dart';
 
-Iterable<Animal> getAnimalsStartingWithL(Iterable<Animal> animals) {
+Iterable<Animal> getAnimalsStartingWithL(IList<Animal> animals) {
   return animals.where((element) {
     return element.name.startsWith("L");
   });
 }
 
-Iterable<Animal> addAnimal(List<Animal> list, Animal animal) {
-  list.add(animal);
-  return list;
+Iterable<Animal> addAnimal(IList<Animal> list, Animal animal) {
+  return list.add(animal);
 }
 
-Iterable<Animal> removeAnimal(List<Animal> list, Animal animal) {
-  list.remove(animal);
-  return list;
+Iterable<Animal> removeAnimal(IList<Animal> list, Animal animal) {
+  return list.remove(animal);
 }
 
 Iterable<Animal> updateAnimalName(
-  List<Animal> list,
+  IList<Animal> list,
   Animal oldAnimal,
   String newName,
 ) {
-  list.remove(oldAnimal);
-  list.add(
+  return list.replaceAllWhere(
+    (element) => element.name == oldAnimal.name,
     Animal(
       name: newName,
       bornAt: oldAnimal.bornAt,
@@ -30,5 +29,4 @@ Iterable<Animal> updateAnimalName(
       lastVisitAt: oldAnimal.lastVisitAt,
     ),
   );
-  return list;
 }

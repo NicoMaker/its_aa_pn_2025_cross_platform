be6a1862688f3f4c2a479d9c96ddf7015c8d2050
~venir <luca.venir@wde.it>
2025-09-30 13:00:11 +0200

    Revert "changed iterables to fit with FIC"
    This reverts commit 0b3bdc2f27e8c81fcdbbab33418d45fc77daf6c0.

--- lib/iterables.dart
+++ lib/iterables.dart
@@ -1,27 +1,28 @@
-import 'package:fast_immutable_collections/fast_immutable_collections.dart';
 import 'package:its_aa_pn_2025_cross_platform/animal.dart';
 
-Iterable<Animal> getAnimalsStartingWithL(IList<Animal> animals) {
+Iterable<Animal> getAnimalsStartingWithL(Iterable<Animal> animals) {
   return animals.where((element) {
     return element.name.startsWith("L");
   });
 }
 
-Iterable<Animal> addAnimal(IList<Animal> list, Animal animal) {
-  return list.add(animal);
+Iterable<Animal> addAnimal(List<Animal> list, Animal animal) {
+  list.add(animal);
+  return list;
 }
 
-Iterable<Animal> removeAnimal(IList<Animal> list, Animal animal) {
-  return list.remove(animal);
+Iterable<Animal> removeAnimal(List<Animal> list, Animal animal) {
+  list.remove(animal);
+  return list;
 }
 
 Iterable<Animal> updateAnimalName(
-  IList<Animal> list,
+  List<Animal> list,
   Animal oldAnimal,
   String newName,
 ) {
-  return list.replaceAllWhere(
-    (element) => element.name == oldAnimal.name,
+  list.remove(oldAnimal);
+  list.add(
     Animal(
       name: newName,
       bornAt: oldAnimal.bornAt,
@@ -29,4 +30,5 @@ Iterable<Animal> updateAnimalName(
       lastVisitAt: oldAnimal.lastVisitAt,
     ),
   );
+  return list;
 }

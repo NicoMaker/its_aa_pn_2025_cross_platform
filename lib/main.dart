7147b58d0fe08359b05163fd1e7bca8c1205c94f
~venir <luca@venir.dev>
2025-10-27 15:58:16 +0100

    extracted a widget to a file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:its_aa_pn_2025_cross_platform/form.dart';
 import 'package:its_aa_pn_2025_cross_platform/todo.dart';
 
 void main() {
@@ -103,114 +104,5 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       _list.add(result);
     });
-  }
-}
-
-class AddTodoFormDialog extends StatefulWidget {
-  const AddTodoFormDialog({super.key});
-
-  @override
-  State<AddTodoFormDialog> createState() => _AddTodoFormDialogState();
-}
-
-class _AddTodoFormDialogState extends State<AddTodoFormDialog> {
-  final _key = GlobalKey<FormState>();
-
-  String? _title;
-  String? _description;
-  bool _isDone = false;
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-
-    return Dialog(
-      child: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Form(
-          key: _key,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text("nuovo todo!", style: theme.textTheme.headlineSmall),
-              SizedBox(height: 40),
-              TextFormField(
-                decoration: InputDecoration(hintText: "titolo..."),
-                validator: (value) {
-                  if (value == null) return "campo obbligatorio!";
-                  if (value.isEmpty) return "campo obbligatorio!";
-                  if (value.length < 3) return "inserisci almeno 3 caratteri";
-                  return null;
-                },
-                onChanged: (value) {
-                  _title = value;
-                },
-              ),
-              TextFormField(
-                decoration: InputDecoration(hintText: "descrizione..."),
-                validator: (value) {
-                  if (value == null) return "campo obbligatorio";
-                  if (value.isEmpty) return "campo obbligatorio!";
-                  if (value.length < 20) return "inserisci almeno 20 caratteri";
-                  return null;
-                },
-                onChanged: (value) {
-                  _description = value;
-                },
-              ),
-              SizedBox(height: 20),
-              FormField<bool>(
-                initialValue: false,
-                validator: (value) {
-                  if (value == true) return null;
-                  return "devi accettare i termini e condizioni";
-                },
-                errorBuilder: (context, errorText) {
-                  print(errorText);
-                  return Text(errorText);
-                },
-                builder: (field) {
-                  return CheckboxListTile(
-                    value: field.value,
-                    contentPadding: EdgeInsets.zero,
-                    title: Text("accetto i t&c"),
-                    onChanged: (value) {
-                      if (value == null) return;
-                      field.didChange(value);
-                      _isDone = value;
-                    },
-                  );
-                },
-              ),
-              SizedBox(height: 80),
-              ElevatedButton(
-                onPressed: () {
-                  final state = _key.currentState?.validate();
-                  if (state == true) {
-                    // è valido!
-                    final todo = Todo(
-                      createdAt: DateTime.now(),
-                      title: _title!,
-                      description: _description!,
-                      isDone: _isDone,
-                    );
-
-                    Navigator.pop(context, todo);
-                  }
-                  if (_title == null) return;
-                  if (_title!.isEmpty) return;
-                  if (_title!.length < 3) return;
-
-                  if (_description == null) return;
-                  if (_description!.isEmpty) return;
-                  if (_description!.length < 20) return;
-                },
-                child: Text("salva!"),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
   }
 }
